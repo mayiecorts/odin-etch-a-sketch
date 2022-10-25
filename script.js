39c93bb7fae60cbfb99d7156170a35594b615d4d
Maylene Cortez <cmaylene145@gmail.com>
2022-10-25 17:02:15 +0800

    Fix repetitive script code

--- script.js
+++ script.js
@@ -9,63 +9,55 @@ const resetBtn = document.querySelector(".reset");
 const createGrid = () => {
   for (i = 0; i <= gridSize; i++) {
     box.innerHTML = "<div class='row'></div>".repeat(i);
-
-    let grids = document.querySelectorAll(".row");
-    grids.forEach((grid) => {
-      grid.innerHTML = "<div class='grid'></div>".repeat(i);
-    });
+    const grids = [...document.querySelectorAll(".row")];
+    grids.map(
+      (grid) => (grid.innerHTML = "<div class='grid'></div>".repeat(i))
+    );
   }
+
+  etchASketch();
 };
 
-//Function to start drawing a sketch
+//Function to start the sketch
 const etchASketch = () => {
-  const pixels = document.querySelectorAll(".grid");
-  pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseover", () => {
-      pixel.style.backgroundColor = "black";
-    });
-  });
+  const pixels = [...document.querySelectorAll(".grid")];
   drawBtn.addEventListener("click", () => {
     drawBtn.style.border = "1px solid #CCC";
     drawBtn.style.borderRadius = "4px";
-  });
-
-  eraseBtn.style.border = "transparent";
-  resetBtn.style.border = "transparent";
-};
-
-//Function to erase each grid using mouseover
-const eraseGrid = () => {
-  const pixels = document.querySelectorAll(".grid");
-  pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseover", () => {
-      pixel.style.backgroundColor = "white";
+    eraseBtn.style.border = "transparent";
+    resetBtn.style.border = "transparent";
+    pixels.map((pixel) => {
+      pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = "black";
+      });
     });
   });
 
   eraseBtn.addEventListener("click", () => {
     eraseBtn.style.border = "1px solid #CCC";
     eraseBtn.style.borderRadius = "4px";
-  });
-
-  drawBtn.style.border = "transparent";
-  resetBtn.style.border = "transparent";
-};
-
-//Function to reset all grids
-const resetGrid = () => {
-  const pixels = document.querySelectorAll(".grid");
-  pixels.forEach((pixel) => {
-    pixel.style.backgroundColor = "white";
+    drawBtn.style.border = "transparent";
+    resetBtn.style.border = "transparent";
+    pixels.map((pixel) => {
+      pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = "white";
+      });
+    });
   });
 
   resetBtn.addEventListener("click", () => {
     resetBtn.style.border = "1px solid #CCC";
     resetBtn.style.borderRadius = "4px";
-  });
 
-  drawBtn.style.border = "transparent";
-  eraseBtn.style.border = "transparent";
+    drawBtn.style.border = "transparent";
+    eraseBtn.style.border = "transparent";
+    pixels.map((pixel) => (pixel.style.backgroundColor = "white"));
+    pixels.map((pixel) => {
+      pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = "white";
+      });
+    });
+  });
 };
 
 //Function to start
